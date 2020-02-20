d1062eaefbe57544a6fdeb9c77572bc499a92a87
Jose Luis Zambrano <zambranojl077@gmail.com>
2020-02-19 20:24:34 -0500

    .

--- app/src/main/java/com/example/parsearjson/SubDatos/BioActivity2.java
+++ app/src/main/java/com/example/parsearjson/SubDatos/BioActivity2.java
@@ -4,7 +4,10 @@ import android.Manifest;
 import android.app.DownloadManager;
 import android.content.Context;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -24,6 +27,36 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.parsearjson.R;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.services.vision.v1.Vision;
+import com.google.api.services.vision.v1.VisionRequestInitializer;
+import com.google.api.services.vision.v1.model.Image;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import com.google.api.services.vision.v1.model.AnnotateImageRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesResponse;
+import com.google.api.services.vision.v1.model.Feature;
+import com.google.api.services.vision.v1.model.Image;
+
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.services.vision.v1.Vision;
+import com.google.api.services.vision.v1.VisionRequestInitializer;
+import com.google.api.services.vision.v1.model.TextAnnotation;
+import com.google.api.services.vision.v1.model.WebDetection;
+import com.google.api.services.vision.v1.model.WebEntity;
+import com.squareup.picasso.Picasso;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+//import org.apache.commons.io.output.ByteArrayOutputStream;
+
+import java.util.Arrays;
+
 
 
 public class BioActivity2 extends AppCompatActivity {
@@ -32,10 +65,16 @@ public class BioActivity2 extends AppCompatActivity {
     ImageView Imagen;
     Button btnDescargar,btnBuscarPortada;
 
+    public Vision vision;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bio2);
+        Vision.Builder visionBuilder = new Vision.Builder(new NetHttpTransport(),
+                new AndroidJsonFactory(),  null);
+        visionBuilder.setVisionRequestInitializer(new
+                VisionRequestInitializer("AIzaSyCV_ADpVQp5_K1CQ98gc6KeOVq5p1sjqKQ"));
+        vision = visionBuilder.build();
 
         String title=(getIntent().getExtras().getString("curTitle"));
         String volum=(getIntent().getExtras().getString("curDate"));
@@ -74,6 +113,61 @@ public class BioActivity2 extends AppCompatActivity {
                     manager.enqueue(request);        }
                 catch (Exception e) {
                     Toast.makeText(BioActivity2.this,e.getMessage(),Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        btnBuscarPortada.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //paso 1
+                TextView tit=findViewById(R.id.txtTitleBio);
+                //paso 2 Feature
+                Feature desiredFeature = new Feature();
+                desiredFeature.setType("WEB_DETECTION");
+
+                // paso 3 arma la sulicitud(es)
+                TextAnnotation request=new TextAnnotation();
+                request.setText(tit.getText().toString());
+                //AnnotateImageRequest request = new AnnotateImageRequest();
+                //request.setImage(inputImage);
+
+                //request.setFeatures(Arrays.asList(desiredFeature));
+                BatchAnnotateImagesRequest batchRequest = new
+                        BatchAnnotateImagesRequest();
+                //batchRequest.setRequests(Arrays.asList(request));
+                //paso 4 asignamos al control visionbuilder la solicitud
+
+                try {
+
+                    Vision.Images.Annotate  annotateRequest =
+                            vision.images().annotate(batchRequest);
+                    //paso 5 enviamos la solicitud
+                    annotateRequest.setDisableGZipContent(true);
+                    BatchAnnotateImagesResponse batchResponse =
+                            annotateRequest.execute();
+
+                    //paso 6 obtener la respuesta
+                    WebDetection annotation = batchResponse.getResponses().get(0).getWebDetection();
+                    //descripcion="";
+                    List<WebEntity> arrayList=annotation.getWebEntities();
+                    final String descripcion=arrayList.get(0).getDescription();
+                    //LlamarWebService(descripcion);
+
+                    final String resultado=descripcion;
+
+                    //paso 7 asignar la UI
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            ImageView imageDetail = (ImageView) findViewById(R.id.txtImagenBio);
+                            //imageDetail.(descripcion);
+                        }
+                    });
+                    //return text.getText();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
         });
